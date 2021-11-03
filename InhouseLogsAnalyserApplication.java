fff51ceac10577185511a054ebf0904ba25fc7e9
akhiljosl <62157739+akhiljosl@users.noreply.github.com>
2021-11-03 18:55:14 +0530

    The starter class for the application

--- InhouseLogsAnalyserApplication.java
+++ InhouseLogsAnalyserApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InhouseLogsAnalyserApplication {
 	public static void main(String[] args) throws FileNotFoundException {
+		
 		SpringApplication.run(InhouseLogsAnalyserApplication.class, args);
 		LogsProcessor reader = new LogsProcessor();
 		reader.fetchFileContent();		
